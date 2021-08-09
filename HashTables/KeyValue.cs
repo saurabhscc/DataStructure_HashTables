0b6418b1dffdb4dc4ae4cc613128f09a8570de95
saurabhs <saurabhpshirsath@gmail.com>
2021-08-09 14:02:35 +0530

    [saurabh] Add :Count Frequency Of Words In Sentence

---
+++ HashTables/KeyValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTables
+{
+    //Value type Data Type KeyValue
+    public struct KeyValue<K, V>
+    {
+        public K Key { get; set; }
+        public V Value { get; set; }
+    }
+
+}
